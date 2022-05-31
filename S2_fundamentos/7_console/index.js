6e61314671e8b93a75982bc76ce1ecf457994b6d
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-05-31 19:30:40 -0300

    Fundamentos - console

---
+++ S2_fundamentos/7_console/index.js
@@ -0,0 +1,27 @@
+const x = 10
+const y = "string"
+const z = [1,2,3]
+
+//concatenação
+console.log(x, y ,z)
+
+//interpolacao
+console.log(`Valor de um inteiro ${x}, uma string "${y}" e valores de um array ${z}`)
+
+//contagem
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+console.count(`O valor de x é ${x}, contagem`)
+
+
+console.count('-')
+console.count('-')
+console.count('-')
+console.count('-')
+console.count('-')
+
+//limpar console
+setTimeout(() => {
+    console.clear()
+}, 2000)
