407019d199a59617130e071ea00c9dcc112365a3
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-05-31 19:43:10 -0300

    Fundamentos - console input

---
+++ S2_fundamentos/9_input_dados_console/index.js
@@ -0,0 +1,11 @@
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+readline.question('Qual sua linguagem preferida? ', (language) => {
+
+    console.log(`Minha linguagem preferida é: ${language}`)
+    readline.close()
+
+})
