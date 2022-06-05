e72d9af2cdd8d5578238a56c87c712333dca6084
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-06-05 13:27:40 -0300

    introducao express - CSS e estaticos

---
+++ S6_int_express/7_css_e_estaticos/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+const usersRouter = require('./users/users')
+
+const path = require('path')
+
+//ler o body
+app.use(
+    express.urlencoded({
+        extended:true
+    }),
+)
+
+app.use(express.json())
+//fim ler body
+
+//arquivos estaticos
+app.use(express.static('public'))
+
+const basePath = path.join(__dirname,'templat')
+
+
+//utiliza o arquivo de rotas
+app.use('/users', usersRouter)
+
+
+app.get('/', (req, res) => {
+    //res.send('Olá Mundo!!!')
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
