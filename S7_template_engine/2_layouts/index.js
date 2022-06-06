94a3db2ae73e698704e4f91c81f28d25663ea636
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-06-06 15:45:32 -0300

    handlebars - setup

---
+++ S7_template_engine/2_layouts/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const port = 3000
+
+app.engine('handlebars', exphbs.engine())
+
+// app.engine(
+//     'handlebars',
+//     exphbs.engine({
+//         extname: "handlebars",
+//         defaultLayout: false,
+//         layoutsDir: "./views"
+//     })
+// );
+
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`Rodando na porta ${port}`)
+})
