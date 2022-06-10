faafb43a2767c225a9dc06efec67aa971a33c73d
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2022-06-10 16:19:15 -0300

    Sequilize - Update

---
+++ S9_sequilize/7_update/db/conn.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize (
+    'nodesequelize',
+    'root',
+    'admin123', {
+        host: 'localhost',
+        dialect: 'mysql'
+    }
+)
+
+// try {
+//     sequelize.authenticate()
+//     console.log('Conectado Sequelize')
+
+// } catch (err) {
+//     console.log('Não foi possível conectar: ', err)
+// }
+
+module.exports = sequelize
